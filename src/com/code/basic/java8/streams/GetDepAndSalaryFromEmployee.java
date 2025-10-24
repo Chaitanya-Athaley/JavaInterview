7a67062d1428c28ebd3d0fae914e5006026ad213
chait <chait@ChaitanyaDev>
2025-10-24 21:19:38 +0530

    GetDepAndSalaryFromEmployee using java 8

---
+++ src/com/code/basic/java8/streams/GetDepAndSalaryFromEmployee.java
@@ -0,0 +1,19 @@
+package com.code.basic.java8.streams;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.code.basic.model.Employee;
+
+public class GetDepAndSalaryFromEmployee {
+
+	public static void main(String[] args) {
+		List<Employee> emplist = Employee.getEmployees();
+		 Map<String, List<Double>> collect = emplist.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.mapping(Employee::getSalary, Collectors.toList() )));
+		 collect.entrySet().forEach(n->{
+			 System.out.println(n.getKey()+" "+n.getValue());
+		 });
+	}
+
+}
