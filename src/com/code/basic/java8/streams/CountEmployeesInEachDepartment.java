b6bc9597b62f9cbd04f725f0f15fdefa04a49aad
Chaitanya-Athaley <chaitnyaathaley@gmail.com>
2025-11-13 01:18:10 +0530

    order

--- src/com/code/basic/java8/streams/CountEmployeesInEachDepartment.java
+++ src/com/code/basic/java8/streams/CountEmployeesInEachDepartment.java
@@ -1,7 +1,9 @@
 package com.code.basic.java8.streams;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import com.code.basic.model.Employee;
@@ -10,8 +12,11 @@ public class CountEmployeesInEachDepartment {
 
 	public static void main(String[] args) {
 		List<Employee> employees = Employee.getEmployees(); 
-		Map<String, Long> collect = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
-		System.out.println(collect);
+		Map<String, Long> insertionOrder = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment,LinkedHashMap::new, Collectors.counting()));
+		System.out.println(insertionOrder);
+		Map<String, Long> normalSortOrder = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment,TreeMap::new, Collectors.counting()));
+		System.out.println(normalSortOrder);
+
 	}
 
 }
